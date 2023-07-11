daeb27d1f7a35722569d0ea44ca01c9e0789241d
gravy-11 <gravy667dev@gmail.com>
2023-07-11 19:47:20 +0900

    Update version to 0.1.0 and add chrome extension files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        background: "./src/background.ts",
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          console.log("chunk==", chunkInfo.facadeModuleId);
+          if (
+            chunkInfo.facadeModuleId &&
+            chunkInfo.facadeModuleId.includes("background.ts")
+          ) {
+            return "background.js";
+          }
+          return "assets/[name].[hash].js";
+        },
+      },
+    },
+  },
+});
